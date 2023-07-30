2b718ee20b0c77dec19637e51a32f841107f31ea
hailuu <hoanghai.luu.71@gmail.com>
2023-07-30 22:56:54 +0700

    update api host

--- LaClient/Materials/ProjectStaticValue.cs
+++ LaClient/Materials/ProjectStaticValue.cs
@@ -2,5 +2,5 @@
 
 public static class ProjectStaticValue
 {
-    public const string Host = "http://ec2-18-141-177-35.ap-southeast-1.compute.amazonaws.com:3003";
+    public const string Host = "http://ec2-18-141-177-35.ap-southeast-1.compute.amazonaws.com:5005";
 }
